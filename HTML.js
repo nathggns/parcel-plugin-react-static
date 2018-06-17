7aecb8e6d6bbb1c9c0005c8c2615ed6439a4c4ff
Nate Higgins <nat@nath.is>
2018-06-17 15:10:30 +0100

    Only process our entry files. Fixes #3

--- HTML.js
+++ HTML.js
@@ -1,29 +1,37 @@
 const React = require('react');
 const Bundler = require('parcel-bundler');
+const logger = require('parcel-bundler/src/Logger');
 const DefaultHTML = require('parcel-bundler/src/packagers/HTMLPackager');
 const { ServerStyleSheet } = require('styled-components');
 const { renderToString } = require('react-dom/server');
 const cheerio = require('cheerio');
 
 module.exports = class extends DefaultHTML {
-  async addAsset(asset) {
-    const assets = this.bundler.loadedAssets;
-    const appPath = Array.from(assets.keys()).find(path => !!path.toLowerCase().match(/app\.js$/));
-    const newBundler = new Bundler(appPath, {});
-    let name;
-    newBundler.on('bundled', bundle => name = bundle.name);
-    await newBundler.bundle();
-    const App = require(name).default;
+  async staticBundle(asset) {
+      const assets = this.bundler.loadedAssets;
+      const appPath = Array.from(assets.keys()).find(path => !!path.toLowerCase().match(/app\.js$/));
+      const newBundler = new Bundler(appPath, {});
+      let name;
+      newBundler.on('bundled', bundle => name = bundle.name);
+      await newBundler.bundle();
+      const App = require(name).default;
 
-    const sheet = new ServerStyleSheet();
-    const markup = renderToString(sheet.collectStyles(
-      React.createElement(App, {}, null)
-    ));
-    const styles = sheet.getStyleTags();
-    const $template = cheerio.load(asset.generated.html);
-    $template('#app').html(markup);
-    $template('head').append(styles);
-    asset.generated.html = $template.html();
+      const sheet = new ServerStyleSheet();
+      const markup = renderToString(sheet.collectStyles(
+          React.createElement(App, {}, null)
+      ));
+      const styles = sheet.getStyleTags();
+      const $template = cheerio.load(asset.generated.html);
+      $template('#app').html(markup);
+      $template('head').append(styles);
+      asset.generated.html = $template.html();
+  }
+
+  async addAsset(asset) {
+    if (this.bundler.entryFiles.includes(asset.name)) {
+      logger.status('💥', `react-static: Beginning static bundle of entry file ${asset.name}`);
+      await this.staticBundle(asset);
+    }
 
     return await super.addAsset(asset);
   }
